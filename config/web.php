88294018a5ba0dd2d20e481b78055b964906b4c1
SatishBanda <30685648+SatishBanda@users.noreply.github.com>
2018-04-23 19:10:30 +0530

    Update web.php

--- config/web.php
+++ config/web.php
@@ -52,8 +52,8 @@ $config = [
         ],
         'sendGrid' => [
             'class' => 'bryglen\sendgrid\Mailer',
-            'username' => 'optionmatrix.auditbench@gmail.com',
-            'password' => 'AuditBench@2018',
+            'username' => 'XXXXX',
+            'password' => 'XXXXXXXXX',
             //'viewPath' => '@app/views/mail', // your view path here
         ],
         'log' => [
